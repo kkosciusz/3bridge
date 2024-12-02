71107b4dd0ee08fdaf3698ce7fa233788f286fde
Krzysztof Kościuszkiewicz <k.kosciuszkiewicz@gmail.com>
2024-12-02 22:16:12 +0100

    hand: implement as_text for Hand

--- src/bridge/hand.py
+++ src/bridge/hand.py
@@ -16,10 +16,48 @@ class Suit(Enum):
         return f'<{self.__class__.__name__}.{self._name_}>'
 
 
+class Rank(Enum):
+    TWO = 0
+    THREE = 1
+    FOUR = 2
+    FIVE = 3
+    SIX = 4
+    SEVEN = 5
+    EIGHT = 6
+    NINE = 7
+    TEN = 8
+    JACK = 9
+    QUEEN = 10
+    KING = 11
+    ACE = 12
+
+    @classmethod
+    def _missing_(cls, value):
+        if isinstance(value, str):
+            return cls._values().index(str)
+        return super()._missing_(value)
+
+    @classmethod
+    def _values(cls):
+        return ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+
+    def __lt__(self, other):
+        if self.__class__ == other.__class__:
+            return self.value < other.value
+        raise NotImplementedError
+
+    def to_text(self):
+        return Rank._values()[self.value]
+
+    @classmethod
+    def from_text(cls, text: str) -> Rank:
+        return cls(cls._values().index(text))
+
+
 @dataclass(eq=True, frozen=True)
 class Card:
     suit: Suit
-    rank: str
+    rank: Rank
 
     @classmethod
     def from_text(cls, text):
@@ -29,7 +67,7 @@ class Card:
             'D': Suit.DIAMOND,
             'C': Suit.CLUB,
         }
-        return cls(suits[text[0]], text[1:])
+        return cls(suits[text[0]], Rank.from_text(text[1:]))
 
     def as_text(self):
         suits = {
@@ -38,14 +76,14 @@ class Card:
             Suit.DIAMOND: 'D',
             Suit.CLUB: 'C',
         }
-        return suits[self.suit] + self.rank
+        return suits[self.suit] + self.rank.to_text()
 
     def hcl(self):
         hcls = {
-            'A': 4,
-            'K': 3,
-            'Q': 2,
-            'J': 1,
+            Rank.ACE: 4,
+            Rank.KING: 3,
+            Rank.QUEEN: 2,
+            Rank.JACK: 1,
         }
         return hcls.get(self.rank, 0)
 
@@ -72,3 +110,26 @@ class Hand:
 
     def hcl(self):
         return sum(card.hcl() for card in self._cards)
+
+    def as_text(self):
+        spades, hearts, clubs, diamonds = [], [], [], []
+        for card in self._cards:
+            if card.suit == Suit.SPADE:
+                spades += [card]
+            if card.suit == Suit.HEART:
+                hearts += [card]
+            if card.suit == Suit.DIAMOND:
+                diamonds += [card]
+            if card.suit == Suit.CLUB:
+                clubs += [card]
+
+        def stringize(cards):
+            ranks = sorted([card.rank for card in cards], reverse=True)
+            return "".join(map(Rank.to_text, ranks))
+
+        spades_str = stringize(spades)
+        hearts_str = stringize(hearts)
+        diamonds_str = stringize(diamonds)
+        clubs_str = stringize(clubs)
+
+        return f'{spades_str}.{hearts_str}.{diamonds_str}.{clubs_str}'
--- tests/test_hand.py
+++ tests/test_hand.py
@@ -1,5 +1,5 @@
 import pytest
-from bridge.hand import Card, Hand, Suit
+from bridge.hand import Card, Hand, Suit, Rank
 
 
 def test_empty_hand_has_len_0():
@@ -9,24 +9,24 @@ def test_empty_hand_has_len_0():
 
 def test_after_adding_card_to_empty_hand_card_is_in_hand():
     hand = Hand()
-    card = Card(Suit.SPADE, 'A')
+    card = Card(Suit.SPADE, Rank.ACE)
     hand.add(card)
     assert card in hand
 
 
 def test_adding_a_card_to_a_hand_twice_throws():
     hand = Hand()
-    card = Card(Suit.SPADE, 'A')
+    card = Card(Suit.SPADE, Rank.ACE)
     hand.add(card)
     with pytest.raises(ValueError, match=r".* already in hand"):
         hand.add(card)
 
 
 def test_comparing_cards_for_equality_is_supported():
-    card1 = Card(Suit.SPADE, 'A')
-    card2 = Card(Suit.HEART, 'A')
-    card3 = Card(Suit.HEART, 'K')
-    card4 = Card(Suit.SPADE, 'A')
+    card1 = Card(Suit.SPADE, Rank.ACE)
+    card2 = Card(Suit.HEART, Rank.ACE)
+    card3 = Card(Suit.HEART, Rank.KING)
+    card4 = Card(Suit.SPADE, Rank.ACE)
 
     assert card1 == card4
     assert card1 != card2
@@ -35,24 +35,50 @@ def test_comparing_cards_for_equality_is_supported():
 
 
 def test_given_a_card_can_compute_hcl_value():
-    ace = Card(Suit.HEART, 'A')
-    king = Card(Suit.SPADE, 'K')
-    queen = Card(Suit.DIAMOND, 'Q')
-    jack = Card(Suit.CLUB, 'J')
-    ten = Card(Suit.CLUB, '10')
+    ace = Card(Suit.HEART, Rank.ACE)
+    king = Card(Suit.SPADE, Rank.KING)
+    queen = Card(Suit.DIAMOND, Rank.QUEEN)
+    jack = Card(Suit.CLUB, Rank.JACK)
+    ten = Card(Suit.CLUB, Rank.TEN)
     cards = (ace, king, queen, jack, ten)
     assert [card.hcl() for card in cards] == [4, 3, 2, 1, 0]
 
 
 def test_given_a_hand_can_compute_hcl_value():
-    ace = Card(Suit.HEART, 'A')
-    king = Card(Suit.SPADE, 'K')
-    queen = Card(Suit.DIAMOND, 'Q')
-    jack = Card(Suit.CLUB, 'J')
-    ten = Card(Suit.CLUB, '10')
+    ace = Card(Suit.HEART, Rank.ACE)
+    king = Card(Suit.SPADE, Rank.KING)
+    queen = Card(Suit.DIAMOND, Rank.QUEEN)
+    jack = Card(Suit.CLUB, Rank.JACK)
+    ten = Card(Suit.CLUB, Rank.TEN)
     cards = (ace, king, queen, jack, ten)
     hand = Hand(cards)
     assert hand.hcl() == 10
+
+
+@pytest.mark.parametrize(
+    ("cards", "expected"),
+    [
+        ([], '...'),
+        (['SA'], 'A...'),
+        (['HA'], '.A..'),
+        (['DA'], '..A.'),
+        (['CA'], '...A'),
+        (['SA', 'HK'], 'A.K..'),
+        (['HA', 'SK'], 'K.A..'),
+        (['DA', 'CK'], '..A.K'),
+        (['CA', 'DK'], '..K.A'),
+        (['CK', 'CA', 'CJ', 'CQ'], '...AKQJ'),
+    ],
+)
+def test_can_output_hand_as_string(cards, expected):
+    hand = Hand({Card.from_text(t) for t in cards})
+    assert hand.as_text() == expected
+
+
+def test_card_ranks_are_properly_ordered():
+    unordered = [Rank.QUEEN, Rank.TWO, Rank.KING, Rank.SIX, Rank.JACK, Rank.ACE, Rank.TEN]
+    ordered = [Rank.TWO, Rank.SIX, Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING, Rank.ACE]
+    assert ordered == sorted(unordered)
 
 
 @pytest.mark.parametrize("rank_text", "A K Q J 10 9 8 7 6 5 4 3 2".split())
